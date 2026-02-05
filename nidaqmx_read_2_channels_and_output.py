ed828779c6b6106e47adcd5daaa242cd3925c3ed
Jerome Fung <jerome.fung@gmail.com>
2026-02-05 16:19:22 -0500

    testing nidaqmx libraries, not sure if this is working yet

---
+++ nidaqmx_read_2_channels_and_output.py
@@ -0,0 +1,55 @@
+import nidaqmx
+from nidaqmx.system import System
+from nidaqmx.stream_readers import AnalogMultiChannelReader
+from nidaqmx.stream_writers import AnalogSingleChannelWriter
+
+system = System.local()
+
+# autodetect device name assuming only 1 is connected
+device_name = system.devices[0].name
+
+# configuration
+sample_rate = 100 # Hz, use this for both analog in and out
+# run for 30 s
+run_time = 30
+n_samples_channel = sample_rate * run_time
+output = np.zeros(n_samples_channel)
+output[100:200] = 5.
+output[300:400] = 5.
+output[600:800] = 5.
+output[900:910] = 5.
+
+
+with nidaqmx.Task() as read_task, nidaqmx.Task() as write_task:
+    read_task.ai_channels.add_ai_voltage_chan(device_name + '/ai0')
+    read_task.ai_channels.add_ai_voltage_chan(device_name + '/ai1')
+    write_task.ao_channels.add_ao_voltage_chan(device_name + '/ao0')
+
+    # Use analog out sample clock to control analog in
+    read_task.timing.cfg_samp_clk_timing(sample_rate, source = device_name + '/ao0',
+                                         samps_per_chan = n_samples_channel)
+    write_task.timing.cfg_samp_clk_timing(sample_rate, samps_per_chan = n_samples_channel)
+
+    # set analog in to trigger the analog out
+    write_task.triggers.start_trigger.cfg_dig_edge_start_trig(trigger_source = device_name + '/ai/StartTrigger')
+
+    # create stream reader/writer object
+    reader = AnalogMultiChannelReader(read_task.in_stream)
+    writer = AnalogSingleChannelWriter(write_task.out_stream)
+
+    input_data = np.zeros((2, n_samples_channel))
+    # documentation says each row corresponds to a channel
+
+    writer.write_many_sample(output)
+    
+    # start tasks
+    write_task.start()
+    read_task.start()
+    reader.read_many_sample(input_data)
+
+    write_task.wait_until_done()
+    read_task.wait_until_done()
+
+    np.save('test_read.npy', input_data)
+
+
